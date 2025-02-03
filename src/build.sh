4128b9b4501932f0e623f6142d5da6eef8f77b9b
loganlieou <logan.lieou@gmail.com>
2025-02-02 23:24:59 -0600

    migrate to dune

--- src/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ocamllex tl13_lex.mll
-ocamlyacc tl13_yac.mly
-ocamlc -c tl13_yac.mli
-ocamlc -c tl13_lex.ml
-ocamlc -c tl13_yac.ml
-ocamlc -c tl13.ml
-ocamlc -o tl13 tl13_lex.cmo tl13_yac.cmo tl13.cmo
